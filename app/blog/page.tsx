3d9b4bc14b42114831372da8584da21bbd2f571d
yikwing <hubeieasy@gmail.com>
2023-04-25 16:18:32 +0800

    feat: 添加prisma db

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,28 @@
-export default function BlogPage() {
-  return <div className="text-blue-500 text-2xl">Blog Index Page</div>;
+import { prisma } from "@/lib/prisma"
+
+const getData = async () => {
+  return {
+    jokeListItems: await prisma.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  }
+}
+
+export default async function BlogPage() {
+  const data = await getData()
+
+  return (
+    <>
+      <div className="text-2xl text-blue-500">Blog Index Page</div>
+      <ul>
+        {data.jokeListItems.map((joke) => (
+          <li key={joke.id}>
+            <p>{joke.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
